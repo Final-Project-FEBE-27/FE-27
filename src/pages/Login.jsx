0aece347ae7e7a8a4fddc73ed9359576859de187
asyamfaiq <asfaiq26@gmail.com>
2022-11-30 19:41:42 +0700

    nama navbar dan logout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,6 +36,7 @@ const Login = () => {
       .then(function (response) {
         console.log('ini response sukses ', response);
         localStorage.setItem('token', response.data.token)
+        localStorage.setItem('username', response.data.data)
         alert("Login sukses")
         navigation('/dashboard')
       })
