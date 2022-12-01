e0078c28f610e69791886a17408d06f96ec881e2
Cynthia <cynthiaparamithaputri@gmail.com>
2022-12-01 22:59:17 +0700

    user edit (incomplete)

--- src/components/admin/UserDetail.jsx
+++ src/components/admin/UserDetail.jsx
@@ -9,6 +9,7 @@ const UserDetail = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    const [user, setUser] = useState([])
     const navigation = useNavigate();
     const { id } = useParams();
     const [isLoading, setisLoading] = useState(true);
@@ -29,7 +30,7 @@ const UserDetail = () => {
       
       var config = {
         method: 'put',
-        url: `https://blue-cloudy-rattlesnake.cyclic.app/admin/editUser/:id`,
+        url: 'https://blue-cloudy-rattlesnake.cyclic.app/admin/user',
         headers: { 
           'Content-Type': 'application/json',
           'auth_token' : token
@@ -49,29 +50,55 @@ const UserDetail = () => {
 
     };
 
-    const getUserById = async () => {
-        const response = await axios.get(`https://blue-cloudy-rattlesnake.cyclic.app/admin/editUser/:id`);
-        setName(response.data.username);
-        setEmail(response.data.email);
-        setPassword(response.data.password);
+    const getUserById = () => {
+      var data = '';
+      
+      var config = {
+        method: 'get',
+        url: 'https://blue-cloudy-rattlesnake.cyclic.app/admin/user',
+        headers: { 
+          'Content-Type': 'application/json',
+          'auth_token' : token
+        },
+        data: data
+      };
+
+      axios(config)
+      .then(function (response) {
+        setUser(response.data.data);
+        //console.log(response.data);
+        setisLoading(false);
+        console.log(response.data.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+        setisError(true);
+      });
+
+
       };
 
 
     return (
-        <>
+        <>{user.filter((el) => el._id === id).map((el, index) => {
+            return (
+            <div key={index}>
             <div className="container edit-content">
                 <h3 className="edit-title mt-6 mb-6">Edit User</h3>
                 <form action="" onSubmit={handleSubmit} className="d-flex flex-column">
                     <label htmlFor="email">Email</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder={email}/>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder={el.email}/>
                     <label htmlFor="password">Password</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder={password}/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder={el.password}/>
                     <label htmlFor="name">Your Name</label>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder={name}/>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder={el.username}/>
                     <button type="submit" className="btn-darker">Update</button>
                     <Link to={"/admin"} className="button is-danger">Cancel</Link> 
                 </form>
             </div>
+            </div>
+            )
+        })}
         </>
     )
 }
